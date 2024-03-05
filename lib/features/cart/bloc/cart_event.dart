8225d4a3fd0de38912c61f37fc070cd99ee61627
kamal <kamalpreet6198@gmail.com>
2024-03-05 15:40:27 +0530

    remove item logic

--- lib/features/cart/bloc/cart_event.dart
+++ lib/features/cart/bloc/cart_event.dart
@@ -5,21 +5,20 @@ sealed class CartEvent {}
 
 class CartInitialevent extends CartEvent {}
 
-class updateTotal extends CartEvent {
- 
-}
+class updateTotal extends CartEvent {}
 
 class IncrementCartItemAmount extends CartEvent {
   final ProductModel productModel;
   IncrementCartItemAmount({
     required this.productModel,
   });
-  
 }
+
 class DecrementCartItemAmount extends CartEvent {
   final ProductModel productModel;
   DecrementCartItemAmount({
     required this.productModel,
   });
-  
 }
+
+
