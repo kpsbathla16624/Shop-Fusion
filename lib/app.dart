4ecbb0f0d4e3c12b5544cd91d2e9c18f2de43354
kamal <kamalpreet6198@gmail.com>
2024-03-05 15:39:56 +0530

    debug banner removed

--- lib/app.dart
+++ lib/app.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.system,
       theme: SfAppTheme.lightTheme,
       //darkTheme: SfAppTheme.DarkTheme,
