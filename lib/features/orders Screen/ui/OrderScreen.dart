63dd491080d580e3a590db248ea3dfbaa53fed53
kamal <kamalpreet6198@gmail.com>
2024-03-07 21:38:48 +0530

    order list tile

--- lib/features/orders Screen/ui/OrderScreen.dart
+++ lib/features/orders Screen/ui/OrderScreen.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:shopfusion/data/repositories/products.dart';
 import 'package:shopfusion/utils/constants/colors.dart';
@@ -13,6 +14,8 @@ class _OrderScreenState extends State<OrderScreen> {
   @override
   void initState() {
     // TODO: implement initState
+    // orders.clear();
+    // saveOrderData(orders);
     super.initState();
   }
 
@@ -23,7 +26,30 @@ class _OrderScreenState extends State<OrderScreen> {
         title: Text('Orders'),
         backgroundColor: SfColor.primary,
       ),
-     
+      body: ListView.builder(
+        itemCount: orders.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                ListTile(
+                  leading: CachedNetworkImage(imageUrl: orders[index].image_path),
+                  title: Text(orders[index].title),
+                  subtitle: Row(
+                    children: [
+                      Text('Rs. ${orders[index].price + 2000} ', style: TextStyle(decoration: TextDecoration.lineThrough),),
+                      SizedBox(width: 10),
+                      Text('Rs. ${orders[index].price}', style: TextStyle(),),
+                    ],
+                  ),
+                ),
+                Divider()
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
