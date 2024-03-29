0e0d28a228ea1f3c42570012e12b4f85c16f02cc
kamal <kamalpreet6198@gmail.com>
2024-03-29 11:43:31 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:shopfusion/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:shopfusion/firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
